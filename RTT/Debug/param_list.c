835aae038c5219ac215f1367f3fdb9b4879f69f5
Faneter <Faneter@outlook.com>
2026-03-20 12:12:48 +0800

    feat: 完善参数表

---
+++ RTT/Debug/param_list.c
@@ -0,0 +1,25 @@
+#include "param_list.h"
+
+#include "string.h"
+
+int Param_Register(const char *name, void *ptr, ParamType_t type, bool is_monitor)
+{
+    if (param_count >= MAX_PARAM_COUNT)
+    {
+        LOG_ERROR("The param_pool exceeds its max limit %d.", MAX_PARAM_COUNT);
+        return -1;
+    }
+
+    Param_t *p = &(param_pool[param_count]);
+    strncpy(p->name, name, 16 - 1);
+    p->name[15] = '\0';
+
+    p->ptr = ptr;
+    p->type = type;
+    p->is_monitor = is_monitor;
+    p->id = param_count;
+
+    param_count++;
+
+    return p->id;
+}
--- RTT/Debug/param_list.h
+++ RTT/Debug/param_list.h
@@ -1,16 +1,39 @@
-#include <stdint.h>
+#pragma once
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <log.h>
+
+#define MAX_PARAM_COUNT 64 // 最大可监测的数据量
+
+/**
+ * @brief 数据类型
+ */
 typedef enum
 {
     TYPE_FLOAT,
     TYPE_INT32,
 } ParamType_t;
 
-typedef struct
+/**
+ * @brief 承载一个参数的结构体
+ *
+ * @param name 参数展示名称
+ * @param id 参数id
+ * @param ptr 要监测的参数
+ * @param type 参数类型
+ * @param is_monitor 是否要高频上传显示波形
+ */
+typedef struct __attribute__((packed))
 {
-    const char *name;
+    char name[16];
     uint8_t id;
     void *ptr;
     ParamType_t type;
-    /* data */
+    bool is_monitor;
 } Param_t;
+
+extern Param_t param_pool[MAX_PARAM_COUNT];
+extern uint8_t param_count;
+
+int Param_Register(const char *name, void *ptr, ParamType_t type, bool is_monitor);
